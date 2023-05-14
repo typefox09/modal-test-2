84784d65c163b3512f9a2515d5330a301add5297
Bobby R <61811299+typefox09@users.noreply.github.com>
2023-05-14 16:38:12 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'modal-test',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
